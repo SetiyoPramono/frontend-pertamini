ae5c2cf2facb7d78c9901b7147c35b49955b489d
SetiyoPramono <setiyopramono04@gmail.com>
2023-01-15 10:39:09 +0700

    -

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,12 +4,12 @@ import SalesChart from '../../src/components/dashboard/SalesChart'
 import TopCards from '../../src/components/dashboard/TopCards'
 import FullLayout from '../../src/layouts/FullLayout'
 
+
 export default function Home() {
   return (
     <div>
         <FullLayout>
-          <SalesChart />
-          <Blog />
+        
         </FullLayout>
     </div>
   )
